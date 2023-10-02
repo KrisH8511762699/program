b9b543a07e98ba8c74d6ee2cfccc08c5fc5238e9
KrisH8511762699 <146726398+KrisH8511762699@users.noreply.github.com>
2023-10-02 17:00:19 +0530

    Initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+int main() {
+    int age;
+    char name;
+    printf("\nenter your age :-  ");
+    scanf("%d",&age);
+    
+    printf("\nenter the name");
+    scanf("%c",&name);
+
+    return 0;
+    
+}
---
+++ krish.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("hello world");
+    scanf("%d",&a);
+    return 0;
+
+
+}
