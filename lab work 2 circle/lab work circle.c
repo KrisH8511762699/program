89afdeae491c6a670d97fc985291d1581420c3dd
KrisH8511762699 <146726398+KrisH8511762699@users.noreply.github.com>
2023-10-05 18:36:01 +0530

    project leb 2

---
+++ lab work 2 circle/lab work circle.c
@@ -0,0 +1,131 @@
+//
+
+#include<stdio.h>
+int main(){
+
+    float pi = 3.14;
+    float radies;
+    float qc;
+
+    printf("enter the value in the circle :-  ");
+    scanf("%f",& radies);
+
+    qc = pi*radies*radies;
+    printf("%f",radies);
+
+
+
+}
+
+//********************************************************************************************************************************************************************
+
+#include<stdio.h>
+int main(){
+
+    int a;
+    int b;
+
+    printf("enter the value :-  ")
+    scanf("%d",&a);
+
+    b = a*a;
+    printf("%d",b)
+
+
+
+}
+
+//********************************************************************************************************************************************************************
+
+#include<stdio.h>
+int main(){
+
+    int a;
+    int b;
+    int c;
+
+    printf("enter the length  :-  ");
+    scanf("%d",&a);
+
+    printf("enter the width :-  ");
+    scanf("%d",&b);
+
+    c = b*a;
+    printf("the ans. is :- %d",c);
+
+
+}
+
+//********************************************************************************************************************************************************************
+
+#include<stdio.h>
+int main(){
+
+    int base;
+    int height;
+    float ans;
+
+    printf("enter the base :-  ");
+    scanf("%d",&base);
+
+    printf("enter the height :-  ");
+    scanf("%d",& height);
+
+    ans = base*height/2;
+    printf("the ans is :-  %f", ans);
+
+
+}
+
+//********************************************************************************************************************************************************************
+
+#include<stdio.h>
+int main(){
+
+
+    int paisa;
+    int rate;
+    int time;
+    float total;
+
+    printf("enter the paisa :-  ");
+    scanf("%d",&paisa);
+
+    printf("enter the rate os instrate :-  ");
+    scanf("%d",&rate);
+
+    printf("enter the time :-  ");
+    scanf("%d",&time);
+
+    total = paisa*rate*time/100;
+    printf("%f mahine me paisa double",total);
+
+    return 0;    
+
+}
+
+//********************************************************************************************************************************************************************
+
+#include<stdio.h>
+int main(){
+
+    float pi = 3.14;
+    float radius;
+    float ans;
+
+    printf("enter the radius :-  ");
+    scanf("%f",&radius);
+
+    ans = 2*pi*radius;
+    printf("the ans is %f",ans);
+
+return 0;
+
+
+}
+
+//********************************************************************************************************************************************************************
+
+
+
+
