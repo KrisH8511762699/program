7799e1795beb42c48bb4a69ac3f684c8032a66c7
KrisH8511762699 <146726398+KrisH8511762699@users.noreply.github.com>
2023-10-06 16:56:14 +0530

    lab work

---
+++ 06-10-2023 if else/lab work.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main(){
+
+    int a;
+    int b;
+    int c;
+
+    printf("enter the first value :-  ");
+    scanf("%d",&a);
+
+    printf("enter the second value :-  ");
+    scanf("%d",&b);
+
+    if(a>b)
+    {
+        printf("a is min. no.");
+    }
+    else
+    {
+        printf("b is min. no.")
+    }
+
+}
+
+//********************************************************************************************************************************************************************
+
+#include<stdio.h>
+int main(){
+
+    int a;
+
+    printf("enter the first value :-  ");
+    scanf("%d",&a);
+    
+    if (a>1){
+        printf("this is natural number ");
+    }
+    else
+    {
+        printf("this no natural number");
+    }
+    
+
+
+}
+
+//********************************************************************************************************************************************************************
